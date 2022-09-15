f9d43a74b148fe13e2c210b8b442aa6f345de3ef
Denys Makene <85587960+Denysmak@users.noreply.github.com>
2022-09-15 09:54:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-
 def idade():
     Rodando = True
     while(Rodando == True):
         nome = input("Escreva o seu nome: ")
+        ano = int(input("Escreva o seu ano de nascimento: "))
+        idade = 2022 - ano
         try:
-            ano = int(input("Escreva o seu ano de nascimento: "))
-            idade = 2022 - ano
             if(ano >= 1922 and ano <= 2021):
                 print("numero válido")
                 Rodando = False
                 print("O seu nome é " + nome + " e você tem " + str(idade) + " anos")
         except:
-            print("erro")
+            print("teste")
 
 
 idade()
