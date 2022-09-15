3c0972bf5c4763436c8344da047ff531ad595986
Denys Makene <85587960+Denysmak@users.noreply.github.com>
2022-09-15 09:43:08 -0300

    Resolve erro do ano

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-#Desenvolva um programa que recebe do usuário nome completo e ano de
-#nascimento que seja entre 1922 e 2021. A partir dessas informações, o
-#sistema mostrará o nome do usuário e a idade que completou, ou completará
-#, no ano atual(2022).
-#Caso o usuário não digite um número ou apareça um inválido no campo do ano
-#, o sistema informará o erro e continuará perguntando até que um valor correto
-#seja preenchido.
-#
+
 def idade():
-    erro = False
-    while(erro == False):
+    Rodando = True
+    while(Rodando == True):
         nome = input("Escreva o seu nome: ")
-        ano = int(input("Escreva o seu ano de nascimento: "))
-        idade = 2022 - ano
         try:
+            ano = int(input("Escreva o seu ano de nascimento: "))
+            idade = 2022 - ano
             if(ano >= 1922 and ano <= 2021):
                 print("numero válido")
-                erro = True
+                Rodando = False
                 print("O seu nome é " + nome + " e você tem " + str(idade) + " anos")
         except:
-            print("teste")
+            print("erro")
 
 
 idade()
